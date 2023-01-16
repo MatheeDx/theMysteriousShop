63804d5c2af3b8dbc9227be36e18d28950da663c
dyrakdebik <mr.xoxol04@mail.ru>
2023-01-16 18:54:52 +0700

    HUD

--- Assets/Vedma/VedmaCore.cs
+++ Assets/Vedma/VedmaCore.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class VedmaCore : MonoBehaviour
 {
+    bool ground, floor, isMove = false;
     [SerializeField] List<Vector2> movePoints;
     Animator anim;
     public float speed;
@@ -14,6 +15,7 @@ public class VedmaCore : MonoBehaviour
 
     private void Start()
     {
+        isMove = true;
         taskShow = false;
         anim = GetComponent<Animator>();
         StartCoroutine(Comein());
@@ -78,5 +80,46 @@ public class VedmaCore : MonoBehaviour
     public int ItemCheck()
     {
         return whatWant.id;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            collision.transform.GetComponent<Sounds>().PlaySteps();
+            ground = true;
+        }
+        else if (collision.transform.tag == "Floor")
+        {
+            Debug.Log("POL");
+            collision.transform.GetComponent<Sounds>().PlaySteps();
+            floor = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (ground == true && collision.transform.tag == "Ground")
+        {
+            collision.transform.GetComponent<Sounds>().StopSteps();
+            ground = false;
+        }
+        else if (ground == true && collision.transform.tag == "Floor")
+        {
+            collision.transform.GetComponent<Sounds>().StopSteps();
+            floor = false;
+        }
+    }
+    public void OnCollisionStay(Collision collision)
+    {
+        if (GetComponent<Player_Move>().isMoving && !isMove)
+        {
+            collision.transform.GetComponent<Sounds>().PlaySteps();
+            isMove = true;
+        }
+        else if (!GetComponent<Player_Move>().isMoving && isMove)
+        {
+            collision.transform.GetComponent<Sounds>().StopSteps();
+            isMove = false;
+        }
     }
 }
